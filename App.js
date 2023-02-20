69998ec3ebda40c5d09dfb544530ffaff9c154e0
peppegiu <34779392+peppegiu@users.noreply.github.com>
2023-02-19 23:39:24 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
